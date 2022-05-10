c1c81bc551c355a9df44624a26d5c58bc49ba1c6
Aïcha <a.diez22@usp.ceu.es>
2022-05-11 01:22:31 +0200

    HomeActivity added

---
+++ app/src/main/java/com/example/recad/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.recad
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about_1)
+
+    }
+}
--- app/src/main/java/com/example/recad/RegistrationActivity.kt
+++ app/src/main/java/com/example/recad/RegistrationActivity.kt
@@ -3,13 +3,29 @@ package com.example.recad
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class RegistrationActivity : AppCompatActivity() {
 
+    private lateinit var backButton: ImageView
+    private lateinit var signInButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        backButton = findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        signInButton = findViewById(R.id.createAccount)
+        signInButton.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
     }
 
     fun changeActivity(view: View){

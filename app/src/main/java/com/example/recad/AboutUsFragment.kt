227224feb182c48581ae0b25bdb0b7a6c460b955
Aïcha <a.diez22@usp.ceu.es>
2022-05-11 01:29:59 +0200

    Update AboutUsFragment.kt

--- app/src/main/java/com/example/recad/AboutUsFragment.kt
+++ app/src/main/java/com/example/recad/AboutUsFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class HomeFragment : Fragment() {
+class AboutUsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = inflater.inflate(R.layout.about_1, container, true)

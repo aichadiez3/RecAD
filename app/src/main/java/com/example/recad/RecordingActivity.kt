d5467d3e6a10b6f6b9b6d3d4ef3c6b3577e8ae25
Aïcha <a.diez22@usp.ceu.es>
2022-05-13 19:23:13 +0200

    x

--- app/src/main/java/com/example/recad/RecordingActivity.kt
+++ app/src/main/java/com/example/recad/RecordingActivity.kt
@@ -12,5 +12,7 @@ class RecordingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recording)
+
+
     }
 }

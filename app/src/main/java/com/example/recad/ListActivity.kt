9edeebb03668faa187a2ff2b26dcb8d90273434b
Aïcha <a.diez22@usp.ceu.es>
2022-06-29 14:21:01 +0200

    List refresh. Done

--- app/src/main/java/com/example/recad/ListActivity.kt
+++ app/src/main/java/com/example/recad/ListActivity.kt
@@ -33,8 +33,10 @@ class ListActivity : AppCompatActivity() {
     private lateinit var pauseButton: ImageView
     private lateinit var list: ListView
     private lateinit var recordsList: MutableList<String>
+    private lateinit var arrayAdapter: ArrayAdapter<*>
 
-
+    private var referenceDir:String = ""
+    private lateinit var localFile: File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,80 +47,63 @@ class ListActivity : AppCompatActivity() {
         pauseButton = findViewById(R.id.pause)
         list  = findViewById(R.id.list)
 
-        mediaPlayer = MediaPlayer()
+
 
         user = FirebaseAuth.getInstance().currentUser!!
 
         database.collection("users").document(user.email.toString()).get().addOnSuccessListener { document ->
             var type = document?.get("diagnosis")?.toString().toString()
-            var referenceDir = "Records/$type"
-        mStorage = FirebaseStorage.getInstance().getReference(referenceDir)
-
-
-        var arrayAdapter: ArrayAdapter<*>
-        recordsList = arrayListOf()
-
-
-        database.collection("users").document(user.email.toString()).collection("records").get().addOnSuccessListener { documents ->
-            for (doc in documents) {
-                var recName = doc.data["record reference"].toString()
-                (recordsList as ArrayList<String>).add(recName)
-            }
-            arrayAdapter = ArrayAdapter(this, R.layout.list_edit_text, recordsList)
-            list.adapter = arrayAdapter
-
-            list.setOnItemClickListener { _, _, position, _ ->
-
-                var audioFound: Boolean
-                var audioRef = recordsList[position]
-
-                val pathReference = mStorage.child("$audioRef") // Create a reference with an initial file path and name
-
-                //var child = "$referenceDir/$audioRef"
-
-                audioRef.replace(".3gp","")
-                val localFile = File.createTempFile(audioRef, "3gp")
-
-
-                try {
-                    pathReference.getFile(localFile).addOnSuccessListener {
-                        Toast.makeText(this, "Local temp file has been created: $localFile", Toast.LENGTH_SHORT).show()
-                        audioFound = true
-
-                        if (audioFound) {
-                            enableReproduction()
-                        } else {
-                            disableAll()
-                        }
-                    }.addOnFailureListener{ ex ->
-                        Log.e(LOG_TAG, "Error creating local file $localFile", ex)
-                    }
-
-
-                } catch(e: Exception){
-                    Log.e(LOG_TAG, "Error creating local file $localFile", e)
-                }
-
-
-                playButton.setOnClickListener {
-                    disableReproduction()
-                    startPlaying(localFile)
-                }
-
-                pauseButton.setOnClickListener {
-                    stopPlaying()
-                    disableAll()
-                    recreate() //Trigger the onCreate method in the activity. Otherwise reproduction doesn't find the file to reproduce
-                }
-
-            }
-        }.addOnFailureListener { exception ->
-            Log.w(TAG, "Error getting documents: ", exception)
-        }
+            referenceDir = "Records/$type"
         }.addOnFailureListener { exception ->
             Log.w(TAG, "Error getting type: ", exception)
         }
+        // Charge data into the list from DB
+        reloadAllData()
 
+
+        list.setOnItemClickListener { _, _, position, _ ->
+
+            var audioFound: Boolean
+            var audioRef = recordsList[position]
+
+            mStorage = FirebaseStorage.getInstance().getReference(referenceDir)
+            val pathReference = mStorage.child("$audioRef") // Create a reference with an initial file path and name
+            localFile = File.createTempFile(audioRef, "3gp")
+
+
+            try {
+                pathReference.getFile(localFile).addOnSuccessListener {
+                    Toast.makeText(this, "Local temp file has been created: $localFile", Toast.LENGTH_SHORT).show()
+                    audioFound = true
+
+                    if (audioFound) {
+                        enableReproduction()
+                    } else {
+                        disableAll()
+                    }
+                }.addOnFailureListener{ ex ->
+                    Log.e(LOG_TAG, "Error creating local file $localFile", ex)
+                }
+
+
+            } catch(e: Exception){
+                Log.e(LOG_TAG, "Error creating local file $localFile", e)
+            }
+
+
+            playButton.setOnClickListener {
+                disableReproduction()
+                var c = localFile.toString()
+                startPlaying(localFile)
+            }
+
+            pauseButton.setOnClickListener {
+                stopPlaying()
+                disableAll()
+                arrayAdapter.clear()
+                reloadAllData()
+            }
+        }
 
 
         homeButton.setOnClickListener {
@@ -128,13 +113,34 @@ class ListActivity : AppCompatActivity() {
 
     }
 
+    private fun reloadAllData() {
+        recordsList = arrayListOf()
+
+        database.collection("users").document(user.email.toString()).collection("records").get().addOnSuccessListener { documents ->
+            for (doc in documents) {
+                var recName = doc.data["record reference"].toString()
+                (recordsList as ArrayList<String>).add(recName)
+            }
+            arrayAdapter = ArrayAdapter(this, R.layout.list_edit_text, recordsList)
+            list.adapter = arrayAdapter
+
+            if(recordsList.isNotEmpty()){
+                list.isEnabled=true
+                increaseAlpha(list)
+            }
+
+        }.addOnFailureListener { exception ->
+            Log.w(TAG, "Error getting documents: ", exception)
+        }
+
+    }
 
     private fun startPlaying(filePath: File) {
 
         list.isEnabled = false
         decreaseAlpha(list)
 
-        mediaPlayer.apply {
+        mediaPlayer = MediaPlayer().apply {
             try {
                 setDataSource(filePath.toString())
                 prepare()
@@ -150,6 +156,8 @@ class ListActivity : AppCompatActivity() {
         mediaPlayer.pause()
         mediaPlayer.release()
     }
+
+
 
 
     private fun increaseAlpha(v: View){
